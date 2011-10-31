d0a3f7352ca3cb72205f25e5923a1ce3f6ede2f4
Samith Sandanayake <sam.the.devel@gmail.com>
2011-10-31 23:48:28 +0530

    code generate wizard ui introduced.

---
+++ src/my2ca/ui/codegen_wiz.py
@@ -0,0 +1,60 @@
+'''
+Created on Oct 31, 2011
+
+@author: sam
+'''
+
+from PySide.QtGui import *
+
+class SelectTablesPage(QWizardPage):
+    
+    def __init__(self):
+        QWizardPage.__init__(self)
+        pass
+    
+    pass
+
+class PreviewCodePage(QWizardPage):
+    
+    def __init__(self):
+        QWizardPage.__init__(self)
+        pass
+    
+    pass
+
+class ProgressPage(QWizardPage):
+    
+    def __init__(self):
+        QWizardPage.__init__(self)
+        pass
+    
+    pass
+
+class FinalizePage(QWizardPage):
+    
+    def __init__(self):
+        QWizardPage.__init__(self)
+        pass
+    
+    pass
+
+class CodegenWiz(QWizard):
+    
+    def __init__(self):
+        QWizard.__init__(self)
+        
+        self.selectTablePage = SelectTablesPage()
+        self.previewCodePage = PreviewCodePage()
+        self.progressPage = ProgressPage()
+        self.finalizePage = FinalizePage()
+        
+        self.addPage(self.selectTablePage)
+        self.addPage(self.previewCodePage)
+        self.addPage(self.progressPage)
+        self.addPage(self.finalizePage)
+        
+        self.setWindowTitle('Generate Code')
+        
+        pass
+    
+    pass
